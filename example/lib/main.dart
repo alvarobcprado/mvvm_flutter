08cd16c7bf947d93af2bed0159a8087cece825b5
Alvaro Prado <alvarob.prado@hotmail.com>
2025-01-01 21:42:17 -0400

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_flutter/mvvm_flutter.dart';
 
-class CalculatorViewModel extends StatefulViewModel<int> {
-  CalculatorViewModel() : super(0);
+class CounterViewModel extends ViewModel {
+  CounterViewModel();
 
   late final increment = Command0(_increment);
   late final decrement = Command0(_decrement);
+  int _counter = 0;
+  int get counter => _counter;
 
   Future<Result<void>> _increment() async {
     await Future.delayed(Duration(seconds: 1));
-    setState(state + 1);
+    notifyView(() => _counter++);
     return Result.success(null);
   }
 
   Future<Result<void>> _decrement() async {
     await Future.delayed(Duration(seconds: 2));
-    setState(state - 1);
+    notifyView(() => _counter--);
     return Result.success(null);
   }
 }
@@ -27,7 +29,7 @@ void main() {
 class MainApp extends StatelessWidget {
   MainApp({super.key});
 
-  final viewModel = CalculatorViewModel();
+  final viewModel = CounterViewModel();
 
   @override
   Widget build(BuildContext context) {
@@ -67,15 +69,15 @@ class MainApp extends StatelessWidget {
           ],
         ),
         body: Center(
-          child: StatefulViewModelBuilder(
+          child: ViewModelBuilder(
             viewModel: viewModel,
-            builder: (context, state) {
+            builder: (context) {
               return Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text('You have pushed the button this many times:'),
                   Text(
-                    '$state',
+                    '${viewModel.counter}',
                     style: Theme.of(context).textTheme.headlineMedium,
                   ),
                 ],
